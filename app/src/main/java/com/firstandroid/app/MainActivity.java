71c0c0c37bd3a3b8cdb31a90957a67d2eb950543
palencia <kelpalencia26@gmail.com>
2022-01-25 01:29:56 +0800

    Second commit

--- app/src/main/java/com/firstandroid/app/MainActivity.java
+++ app/src/main/java/com/firstandroid/app/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "9d279194-325e-4491-ba5c-74c23b7f5bd7",
+                Analytics.class, Crashes.class);
     }
 }
